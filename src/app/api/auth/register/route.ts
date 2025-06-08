72581a94c7e468c3eea90a224a4d486aa0087d09
prakhar17072004 <pmankur2004@gmail.com>
2025-06-08 23:17:48 +0530

    backend is added

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,39 +1,41 @@
-import { NextRequest,NextResponse } from "next/server";
-import {db} from '@/db/index';
-import {usersTable} from '@/db/schema';
-import {eq} from "drizzle-orm"
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/db/index";
+import { usersTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import bcrypt from "bcrypt";
 
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Email and password  are required" },
+        { status: 400 }
+      );
+    }
+    const existingUser = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.email, email));
+    if (existingUser.length >0 ) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 409 }
+      );
+    }
 
-export async function POST(request:NextRequest) {
-
-    try {
-         const {email,password}= await request.json();
-         if(!email|| !password){
-            return NextResponse.json(
-                {error:"Email and password  are required"},
-                {status:400}
-            )
-         }
-       const existingUser = await db.select().from(usersTable).where(eq(usersTable.email,email));
-      if(existingUser){
-        return NextResponse.json(
-            { error: "User already exists" },
-            { status: 409 }
-          );
-       }
-      await db.insert(usersTable).values({ email, password });
+    //hashed the password
+    const hashedPassword = await bcrypt.hash(password, 10); 
+    await db.insert(usersTable).values({ email, password: hashedPassword });
 
     return NextResponse.json(
-        { message: "User registered successfully" },
-        { status: 201 }
-      );
-         
-    } catch (error) {
-        return NextResponse.json(
-            { error: " fail to  register User successfully" },
-            { status: 500 }
-          );
-        
-    }
-    
+      { message: "User registered successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: " fail to  register User successfully" },
+      { status: 500 }
+    );
+  }
 }
